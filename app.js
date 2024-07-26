704c9775af1f50a1230d6439bb5ea19d3ea85cf7
Oleg Voitiuk <olgv@protonmail.com>
2024-07-26 14:44:16 +0300

    Create new file

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const fs = require('fs/promises');
 
 (async () => {
+  const createFile = async (path) => {
+    try {
+      const existingFileHandler = await fs.open(path, 'r');
+      await existingFileHandler.close();
+      // If there is no error - file already exists
+      return console.log(`File already exists at ${path}`)
+    } catch (error) {
+      const newFileHandler = await fs.open(path, 'w');
+      console.log(`File created at ${path}`);
+      await newFileHandler.close();
+    }
+  };
+  const commands = {
+    CREATE_FILE: 'create file',
+  };
   const commandFilePath = './command.txt';
   const commandFileHandler = await fs.open(commandFilePath, 'r');
 
@@ -11,8 +26,14 @@ const fs = require('fs/promises');
     const length = buff.byteLength; // How many bytes we want to read
     const position = 0; // always read from the first character
 
-    const content = await commandFileHandler.read(buff, offset, length, position);
-    console.log(buff.toString('utf-8'));
+    await commandFileHandler.read(buff, offset, length, position);
+    const command = buff.toString('utf-8');
+
+    if (command.includes(commands.CREATE_FILE)) {
+      const filePath = command.substring(commands.CREATE_FILE.length + 1);
+
+      await createFile(filePath);
+    }
   })
 
   const watcher = await fs.watch(commandFilePath);
