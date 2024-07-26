6418c95918917be78ce02e2adcbef5abaf9c5028
Oleg Voitiuk <olgv@protonmail.com>
2024-07-26 13:54:30 +0300

    Refactor

--- app.js
+++ app.js
@@ -3,20 +3,23 @@ const fs = require('fs/promises');
 (async () => {
   const commandFilePath = './command.txt';
   const commandFileHandler = await fs.open(commandFilePath, 'r');
+
+  commandFileHandler.on('change', async () => {
+    const size = (await commandFileHandler.stat()).size;
+    const buff = Buffer.alloc(size);
+    const offset = 0; // the location at which we want to start filling our buffer
+    const length = buff.byteLength; // How many bytes we want to read
+    const position = 0; // always read from the first character
+
+    const content = await commandFileHandler.read(buff, offset, length, position);
+    console.log(content);
+  })
+
   const watcher = await fs.watch(commandFilePath);
 
   for await (const event of watcher) {
     if (event.eventType === 'change') {
-      console.log('File changed');
-
-      const size = (await commandFileHandler.stat()).size;
-      const buff = Buffer.alloc(size);
-      const offset = 0;
-      const length = buff.byteLength;
-      const position = 0; // always read from the first character
-
-      const content = await commandFileHandler.read(buff, offset, length, position);
-      console.log(content);
+      commandFileHandler.emit('change');
     }
   }
 })();
